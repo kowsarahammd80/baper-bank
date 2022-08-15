0105f8582936c9c7bd155bee4ff45ffa6bab31af
kowsarahammed80 <kowsarahammed80@gmail.com>
2022-08-15 22:45:31 +0600

    baper-bank

---
+++ js/login.js
@@ -0,0 +1,15 @@
+// step-1: add click event handler with the submit button
+ document.getElementById('btn-submit').addEventListener('click', function(){
+  // step-2 get the email address inside the email input field
+  // always remeber to use .velue to get text from an input firld and textarea tege.
+  let emailField = document.getElementById('enter-email');
+  let email = emailField.value;
+  console.log(email);
+  //step-3 get password
+  //3.a set id on the html element
+  //3.b get the element 
+  //3.c get the value from the element
+
+  
+
+})
