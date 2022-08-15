3f5db42678b58e9ee2eace37360df5beb1db4436
kowsarahammed80 <kowsarahammed80@gmail.com>
2022-08-15 23:02:50 +0600

    loging

--- js/login.js
+++ js/login.js
@@ -4,12 +4,24 @@
   // always remeber to use .velue to get text from an input firld and textarea tege.
   let emailField = document.getElementById('enter-email');
   let email = emailField.value;
-  console.log(email);
+  
   //step-3 get password
   //3.a set id on the html element
   //3.b get the element 
   //3.c get the value from the element
 
+  let passwordField = document.getElementById('user-password');
+  let password = passwordField.value;
   
+  // donot verify email password on the client side
+  //step-4: verify email and password and check wether valid user or not
+  if(email === 'sontan@baap.com' && password === 'secret'){
+    console.log('valid user')
+  }
+  else{
+    console.log('invalid user');
+  }
+  
+
 
 })
